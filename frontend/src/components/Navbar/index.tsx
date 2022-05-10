0b27070491fe50c8ee811d4bd7b71461ccd589e5
Rafael Casotti <rafa_scasotti@hotmail.com>
2022-05-10 18:06:37 -0300

    @RafaelCasotti [Task: Navbar]

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import {ReactComponent as GithubIcon} from 'assets/img/Github.svg.svg';
+import './styles.css';
+
+function Navbar(){
+
+    return (
+        <header>
+      <nav className='container'>
+        <div className='dsmovie-nav-content'>
+        <h1>DSMovie</h1>
+        <a href="https://github.com/RafaelCasotti">
+          <div className='dsmovie-contact-container'><GithubIcon />
+          <p className='dsmovie-contact-link'>/RafaelCasotti</p>
+          </div>
+        </a>
+        </div>
+      </nav>
+    </header>
+    )
+}
+
+export default Navbar;
