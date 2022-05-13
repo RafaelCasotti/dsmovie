6de427b7acea706dd21eb54089876201bc241386
Rafael Casotti <rafa_scasotti@hotmail.com>
2022-05-13 14:41:45 -0300

    @RafaelCasotti [Task: Alteração]

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ function Navbar(){
       <nav className='container'>
         <div className='dsmovie-nav-content'>
         <h1>DSMovie</h1>
-        <a href="https://github.com/RafaelCasotti">
+        <a href="https://github.com/RafaelCasotti" title="Github" target="_blank" rel="noopener noreferrer">
           <div className='dsmovie-contact-container'><GithubIcon />
           <p className='dsmovie-contact-link'>/RafaelCasotti</p>
           </div>
